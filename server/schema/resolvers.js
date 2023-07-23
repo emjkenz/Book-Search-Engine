39e669ab34dcab5252ba217b9c6ce1ee05fed0b8
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-07-24 01:33:53 +1000

    Fixed resolvers

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,13 +1,97 @@
-// Replace this with your actual logic to fetch user data from the database
-const mockUserData = {
-    _id: '123',
-    username: 'john_doe',
-    email: 'john@example.com',
-};
+const { User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        me: () => mockUserData,
+        me: async (parent, args, context) => {
+            if (context.user) {
+                try {
+                    const user = await User.findById(context.user._id);
+                    return user;
+                } catch (error) {
+                    console.error(error);
+                    throw new Error('Failed to fetch user data.');
+                }
+            } else {
+                return null;
+            }
+        },
+    },
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            try {
+                const user = await User.findOne({ email });
+
+                if (!user) {
+                    throw new AuthenticationError('Incorrect email or password.');
+                }
+
+                const correctPassword = await user.isCorrectPassword(password);
+
+                if (!correctPassword) {
+                    throw new AuthenticationError('Incorrect email or password.');
+                }
+
+                const token = signToken(user);
+
+                return { token, user };
+            } catch (error) {
+                console.error(error);
+                throw new Error('Failed to log in.');
+            }
+        },
+        addUser: async (parent, args) => {
+            try {
+                const newUser = await User.create(args);
+                const token = signToken(newUser);
+                return { token, user: newUser };
+            } catch (error) {
+                console.error(error);
+                throw new Error('Failed to create a new user.');
+            }
+        },
+        saveBook: async (parent, args, context) => {
+            if (context.user) {
+                try {
+                    const user = await User.findById(context.user._id);
+
+                    user.savedBooks.push({
+                        bookId: args.bookId,
+                        authors: args.authors,
+                        description: args.description,
+                        title: args.title,
+                        image: args.image || '',
+                        link: args.link || '',
+                    });
+
+                    const updatedUser = await user.save();
+                    return updatedUser;
+                } catch (error) {
+                    console.error(error);
+                    throw new Error('Failed to save the book.');
+                }
+            } else {
+                throw new AuthenticationError('You need to be logged in to save a book.');
+            }
+        },
+        removeBook: async (parent, args, context) => {
+            if (context.user) {
+                try {
+                    const user = await User.findById(context.user._id);
+
+                    user.savedBooks = user.savedBooks.filter((book) => book.bookId !== args.bookId);
+
+                    const updatedUser = await user.save();
+                    return updatedUser;
+                } catch (error) {
+                    console.error(error);
+                    throw new Error('Failed to remove the book.');
+                }
+            } else {
+                throw new AuthenticationError('You need to be logged in to remove a book.');
+            }
+        },
     },
 };
 
