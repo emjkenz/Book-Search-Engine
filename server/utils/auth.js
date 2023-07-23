7cb22cb875eb9fc25e1b02bd9869fc0e7281dd72
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-07-23 23:52:23 +1000

    Refactored auth for graphql

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -7,29 +7,29 @@ const expiration = '2h';
 module.exports = {
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
-
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
-    }
+    // allows token to be sent via headers
+    const token = req.headers.authorization;
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return res.status(401).json({ errors: [{ message: 'Authentication token missing' }] });
+    }
+
+    // Token format: "Bearer <tokenvalue>"
+    const tokenValue = token.split(' ')[1];
+
+    if (!tokenValue) {
+      return res.status(401).json({ errors: [{ message: 'Invalid token format' }] });
     }
 
     // verify token and get user data out of it
     try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      const { data } = jwt.verify(tokenValue, secret, { maxAge: expiration });
       req.user = data;
-    } catch {
+      next();
+    } catch (error) {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      return res.status(401).json({ errors: [{ message: 'Invalid token' }] });
     }
-
-    // send to next endpoint
-    next();
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
