c9a25bf5b7fa06d8697ea5561987a2338826dac4
Em Kennedy <36625564+emjkenz@users.noreply.github.com>
2023-07-24 00:12:41 +1000

    Fixed the typedef inputs

--- server/server.js
+++ server/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
-const typeDefs = require('./schema');
-const resolvers = require('./resolvers');
+const { typeDefs, resolvers } = require('./schema');
 const db = require('./config/connection');
 const routes = require('./routes');
 
